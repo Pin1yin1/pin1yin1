4feb5529f6310b6bb7e7889a6b110639c9d6c97a
Tom Fifield <tom@openstack.org>
2017-01-14 13:18:56 +0800

    Fix: Passing conditions to delete_all is deprecated
    Passing conditions to delete_all is deprecated and will be removed in Rails 5.1
    
    This patch changes these statements to instead be the recommended:
    
     where(conditions).delete_all.

--- script/import/import_cedict.rb
+++ script/import/import_cedict.rb
@@ -30,7 +30,7 @@ end
 
 
 file = File.open(Rails.root+"data/cedict_ts.u8")
-Definition.delete_all(:active => false)
+Definition.where(:active => false).delete_all
 line_num = 0
 sql = nil
 while line = file.gets
@@ -107,5 +107,5 @@ end
 
 puts ""
 Definition.connection.execute "update definitions set active = !active"
-Definition.delete_all :active => false
+Definition.where(:active => false).delete_all
 Definition.connection.execute 'alter table definitions enable keys'
